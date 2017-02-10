75f025612ba9d0f68e6bd319d5a900e7ef69c82f
Manh Le <quang.manh@gmail.com>
2017-02-09 22:25:46 -0600

    Change Build

--- makerelease.bat
+++ makerelease.bat
@@ -3,7 +3,7 @@
 SET COMPILE="C:\Users\Manh Le\Desktop\Mini JavaScript\compiler.jar"
 SET JAVA="C:\Program Files (x86)\Java\jre1.8.0_65\bin\java.exe"
 
-SET DEST = release
+SET DEST=release
 SET DSTBROWSERPATH=%DEST%\browser
 SET DSTNODEPATH=%DEST%\nodejs
 
@@ -18,6 +18,6 @@ xcopy %SRCBROWSERPATH%\async8.js %DSTBROWSERPATH% /y
 %JAVA% -jar %COMPILE%  --compilation_level SIMPLE_OPTIMIZATIONS --js %SRCNODEPATH%\index.js --js_output_file %DSTNODEPATH%\index.js
 %JAVA% -jar %COMPILE% --compilation_level SIMPLE_OPTIMIZATIONS --js %SRCNODEPATH%\MA.js --js_output_file %DSTNODEPATH%\MA.js
 
-xcopy "README.md" .\%DEST% /y
+xcopy ".\README.md" .\%DEST% /y
 
 echo ------Completed------
